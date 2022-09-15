fa8cb8739dc24294d84dcb9d4cf38f0b7e5d602f
Antonio Kobashikawa <akobashikawa@gmail.com>
2022-09-15 12:51:30 -0500

    Establece status 0

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const https = require("https");
 const fs = require("fs");
 const express = require('express');
+const { throws } = require("assert");
 const app = express();
 const PORT = 3030;
 
@@ -18,7 +19,13 @@ app.post('/', (req, res) => {
     console.log('POST...');
     console.log('req.httpVersion', req.httpVersion);
     console.log('req.rawHeaders', req.rawHeaders);
-    // res.send("POST Request Called\n");
+    try {
+      console.log('try');
+      res.status(0).send('OK');
+    } catch (error) {
+      console.log('catch');
+    }
+    res.status(0).send("POST Request Called\n");
 });
 
 const options = {
@@ -31,3 +38,7 @@ https
   .listen(PORT, ()=>{
     console.log("Server listening on PORT", PORT)
   });
+
+// app.listen(PORT, ()=>{
+//   console.log("Server listening on PORT", PORT)
+// })
