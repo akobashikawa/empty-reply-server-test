1fcd28873a3f700cdace18954cebe51e6ba60be3
Antonio Kobashikawa <akobashikawa@gmail.com>
2022-09-15 13:07:10 -0500

    Agrega soporte http2

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const https = require("https");
+const http2Express = require('http2-express-bridge');
+const http2 = require("http2");
 const fs = require("fs");
 const express = require('express');
 const { throws } = require("assert");
-const app = express();
+const app = http2Express(express);
 const PORT = 3030;
 
 app.use(express.json());
@@ -33,11 +35,12 @@ const options = {
     cert: fs.readFileSync("cert.pem"),
 };
 
-https
-  .createServer(options, app)
-  .listen(PORT, ()=>{
-    console.log("Server listening on PORT", PORT)
-  });
+const server = http2
+  .createSecureServer(options, app);
+
+server.listen(PORT, ()=>{
+  console.log("Server listening on PORT", PORT);
+});
 
 // app.listen(PORT, ()=>{
 //   console.log("Server listening on PORT", PORT)
